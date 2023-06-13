eef6f6d8c3857ba27d77a571180ecf9d2f337ab7
reyot03 <50517664+reyot03@users.noreply.github.com>
2023-06-13 19:32:58 +0545

    Add files via upload

---
+++ SITES.py
@@ -0,0 +1,11 @@
+from MnMdomains import *
+
+SITE_NAMES = [
+    'mangago',
+    'mangakakalot',
+    'mangageko',
+    'manga18fx',
+    'webtoons',
+]
+
+SITE_CLASSES = [Mangago, Mangakakalot, Mangageko, Webtoons, Manga18fx]
