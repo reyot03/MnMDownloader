5ae885c7dba89060cd66a58abe980ff772ec71e8
reyot03 <50517664+reyot03@users.noreply.github.com>
2023-06-29 14:50:23 +0545

    urllib.request removed

--- mnmdomains.py
+++ mnmdomains.py
@@ -1,8 +1,8 @@
 from io import BytesIO
 from PIL import Image
 import re
-from requests_html import AsyncHTMLSession
-from urllib import request, parse, error
+from requests_html import AsyncHTMLSession, HTMLSession
+from urllib import parse
 from bs4 import BeautifulSoup as bs
 from pathlib import Path
 import time
@@ -16,28 +16,35 @@ import math
 
 class Manga_Site:
     def __init__(self):
-        self.HEADERS = {
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
         self.manga_selected = None
         self.manga_name = None
         self.pdf_request = None
         self.img_request = None
         self.manga_list = []
         self.chapter_list = []
+        self.session = HTMLSession()
 
     def filter_input(self):
         self.manga_search_input = self.manga_search_input.replace(' ', '+')
-        self.manga_search_input = re.sub(
-            r'[^+\-\w]+', '', self.manga_search_input)
+        self.manga_search_input = re.sub(r'[^+\-\w]+', '', self.manga_search_input)
+
+    def request_page(self, url):
+        try:
+            res = self.session.get(url)
+            self.base_url = res.html.base_url
+            return res.content
+        except:
+            print("Couldn't connect to server")
+            exit()
 
     def search_manga(self, search_input):
         self.manga_search_input = search_input
         self.filter_input()
         print(f'\nSearching for {self.manga_search_input.replace("+", " ")} in {self.site_name}')
         search_url = self.base_manga_search_url.format(self.manga_search_input)
-        html_text = self.request_page(search_url)
+        html_content = self.request_page(search_url)
         # scrap manga names, chapter-urls, and latest chapter name
-        soup = bs(html_text, "html.parser")
+        soup = bs(html_content, "html.parser")
         m_container = soup.find(class_=self.manga_container_class).find_all('li')
         try:
             for m in m_container:
@@ -55,29 +62,8 @@ class Manga_Site:
                 except:
                     continue
                 self.manga_list.append(manga)
-        except TypeError:
-            print("Manga not found. Try changing the keyword")
-            exit()
         except:
-            print("Something went wrong while getting manga link")
-            exit()
-        if len(self.manga_list) == 0:
-            print("Manga not found. Try changing the keyword")
-
-    def request_page(self, url):
-        connection_error_count = 0
-        while True:
-            try:
-                req = request.Request(url, headers=self.HEADERS)
-                resp = request.urlopen(req)
-                return resp.read()
-            except:
-                connection_error_count += 1
-                if connection_error_count > 10:
-                    exit()
-                print(
-                    f"Connection Error Occured {connection_error_count} times")
-                time.sleep(1)
+            print("Site changed")
 
     def search_chapters(self, manga_selected):
         self.manga_selected = manga_selected
@@ -86,10 +72,10 @@ class Manga_Site:
         # select manga url from manga-list using manga_selected integer
         search_url = self.manga_list[self.manga_selected]["url"]
         if self.site_name == "mangageko":
-            search_url += "all-chapters"
-        html_text = self.request_page(search_url)
+            search_url = parse.urljoin(search_url, "/all-chapters")
+        html_content = self.request_page(search_url)
         # scrap chapter names and urls from that manga url
-        soup = bs(html_text, "html.parser")
+        soup = bs(html_content, "html.parser")
         ch_container = soup.find(class_=self.chapter_container_class).find_all(self.chapter_tag)
         try:
             index = 1
@@ -102,25 +88,16 @@ class Manga_Site:
                 self.chapter_list.insert(0, chapter)
                 index += 1
         except TypeError:
-            print("No Chapters Found")
-            exit()
-        except:
-            print("Something went wrong while getting chapters")
-            exit()
-        if len(self.chapter_list) == 0:
-            print("No Chapters Found")
-            exit()
+            print("Site changed")
 
-    @staticmethod
-    async def get_urls(asession, chapter_url):
-        res = await asession.get(chapter_url)
+    async def get_urls(self, chapter_url):
+        res = await self.session.get(chapter_url)
         imgs = res.html.find("#chapter-reader > img")
         img_urls = [img.attrs['src'] for img in imgs]
         return img_urls
 
-    @staticmethod
-    async def download_image(asession, img_url):
-        res = await asession.get(img_url)
+    async def download_image(self, img_url):
+        res = await self.session.get(img_url)
         image_content = Image.open(BytesIO(res.content))
         return image_content
 
@@ -151,12 +128,12 @@ class Manga_Site:
         for pg, im in enumerate(images):
             im.save(Path(ch_path, f'{pg+1}.{im.format}'))
 
-    async def download_ch(self, asession, chapter_name, chapter_url, manga_path):
+    async def download_ch(self, chapter_name, chapter_url, manga_path):
         print('Downloading', chapter_name)
-        img_urls = await self.get_urls(asession, chapter_url)
+        img_urls = await self.get_urls(chapter_url)
         images = []
         for img_url in img_urls:
-            images.append(await self.download_image(asession, img_url))
+            images.append(await self.download_image(img_url))
         # creating pdf and images
         ch_path = Path(manga_path, chapter_name)
         pdf_path = f'{ch_path}.pdf'
@@ -171,16 +148,13 @@ class Manga_Site:
         manga_path = Path('Downloads', self.manga_name)
         manga_path.mkdir(parents=True, exist_ok=True)
         # using requests-html asynchronous session
-        asession = AsyncHTMLSession()
+        self.session.close()
+        self.session = AsyncHTMLSession()
         tasks = [lambda chapter=chapter:self.download_ch(
-            asession, chapter["name"], chapter["url"], manga_path) for chapter in self.chapter_list[st_index:(end_index+1)]]
-        try:
-            st_time = time.perf_counter()
-            asession.run(*tasks)
-            time_taken = time.perf_counter() - st_time
-        except error.URLError:
-            print("Couldn't connect to server")
-            exit()
+            chapter["name"], chapter["url"], manga_path) for chapter in self.chapter_list[st_index:(end_index+1)]]
+        st_time = time.perf_counter()
+        self.session.run(*tasks)
+        time_taken = time.perf_counter() - st_time
         print(f'\nDownload time: {time_taken:.2f}s')
 
 
@@ -188,9 +162,7 @@ class Mangago(Manga_Site):
     def __init__(self):
         super().__init__()
         import utils.mangagoKey
-        (self.key, self.iv) = utils.mangagoKey.get_key_iv()
-        self.key = bytes.fromhex(self.key)
-        self.iv = bytes.fromhex(self.iv)
+        self.key, self.iv = utils.mangagoKey.get_key_iv(self.session)
         # site information to scrap data
         self.site_name = "mangago.me"
         self.base_url = "https://www.mangago.me"
@@ -203,7 +175,7 @@ class Mangago(Manga_Site):
         self.chapter_tag = 'tr'
 
     @staticmethod
-    async def unscramble_image(url, res):
+    def unscramble_image(url, res):
         IMGKEYS = constants.IMGKEYS
         MK1 = constants.MK1
         MK2 = constants.MK2
@@ -263,13 +235,13 @@ class Mangago(Manga_Site):
             return im
         return im_new
 
-    async def download_image(self, asession, img_url):
-        res = await asession.get(img_url)
-        image_content = await self.unscramble_image(img_url, res)
+    async def download_image(self, img_url):
+        res = await self.session.get(img_url)
+        image_content = self.unscramble_image(img_url, res)
         return image_content
 
-    async def get_urls(self, asession, chapter_url):
-        res = await asession.get(chapter_url)
+    async def get_urls(self, chapter_url):
+        res = await self.session.get(chapter_url)
         script = res.html.find('script', containing='imgsrcs', first=True)
         imgsrcs = script.search('imgsrcs = {};')[0][1:-1]
         imgsrcs = base64.b64decode(imgsrcs)
--- utils/mangagoKey.py
+++ utils/mangagoKey.py
@@ -1,41 +1,31 @@
-from urllib import request
-from bs4 import BeautifulSoup as bs
 import re
 
 
-HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
-
-def get_page(chap_url):
-	req = request.Request(chap_url, headers=HEADERS)
-	resp = request.urlopen(req)
-	html_text = resp.read()
-	return html_text
-
 def decode_sojson_v4(jsf):
     head = "['sojson.v4']"
-    jsf = str(jsf)
+    if type(jsf) == bytes:
+        jsf = jsf.decode()
     if head not in jsf:
         return False
     args = re.findall(r'\d+', jsf[240:-58])
     text = ''.join(map(lambda x: chr(int(x)), args))
     return text
 
-def get_key_iv():
-	chap_url = "https://www.mangago.me/read-manga/onepunch_man/mh/c001/"
-	# get html page
-	html_text = get_page(chap_url)
 
-	# extract script_url containing sojson.v4 encoded key and iv
-	soup = bs(html_text, 'html.parser')
-	pattern = re.compile(r"https://.+/chapter\.js.+")
-	script_url = soup.find("script", src=pattern)["src"]
-
-	# decrypt sojson.v4 script and find key and iv
-	encrypted_script = get_page(script_url).decode()
-	decripted_script = decode_sojson_v4(encrypted_script)
-	if not decripted_script:
-		return ('e11adc3949ba59abbe56e057f20f883e', '1234567890abcdef1234567890abcdef')
-	key = re.search(r'var key = CryptoJS.enc.Hex.parse\(\W(.*?)\W\);'.replace(' ', r'\W+'), decripted_script).group(1)
-	iv = re.search(r'var iv = CryptoJS.enc.Hex.parse\(\W(.*?)\W\);'.replace(' ', r'\W+'), decripted_script).group(1)
-
-	return (key, iv)
+def get_key_iv(session):
+    chap_url = "https://www.mangago.me/read-manga/onepunch_man/mh/c001/"
+    res = session.get(chap_url)
+    # extract script_url containing sojson.v4 encoded key and iv
+    scripts = res.html.find('script[src]')
+    script_url = [s.attrs['src'] for s in scripts if 'chapter.js' in s.html][0]
+    # decrypt sojson.v4 script and find key and iv
+    encrypted_script = session.get(script_url).content
+    if not encrypted_script:
+        print("using custom key")
+        return (b'\xe1\x1a\xdc9I\xbaY\xab\xbeV\xe0W\xf2\x0f\x88>', b'\x124Vx\x90\xab\xcd\xef\x124Vx\x90\xab\xcd\xef')
+    decripted_script = decode_sojson_v4(encrypted_script)
+    key = re.search(r'key\W+=\W+CryptoJS.enc.Hex.parse\(\W(.*?)\W\);', decripted_script).group(1)
+    iv = re.search(r'iv\W+=\W+CryptoJS.enc.Hex.parse\(\W(.*?)\W\);', decripted_script).group(1)
+    key = bytes.fromhex(key)
+    iv = bytes.fromhex(iv)
+    return (key, iv)
